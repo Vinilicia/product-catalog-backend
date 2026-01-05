4088b2ddaf14a786908a6653583d1835c904ce0e
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-01-05 15:29:04 -0300

    name is unique

--- src/main/java/com/vinilicia/catalog_backend/domain/model/Person.java
+++ src/main/java/com/vinilicia/catalog_backend/domain/model/Person.java
@@ -12,7 +12,7 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
 
     protected Person() {}
