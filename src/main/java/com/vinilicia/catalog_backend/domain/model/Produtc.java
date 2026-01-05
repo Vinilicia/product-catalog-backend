55e768abf1af4be0fd31c9d2af3dc8a840ddbcd9
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-01-05 18:41:14 -0300

    added local and categories variables

--- src/main/java/com/vinilicia/catalog_backend/domain/model/Produtc.java
+++ src/main/java/com/vinilicia/catalog_backend/domain/model/Produtc.java
@@ -1,10 +1,8 @@
 package com.vinilicia.catalog_backend.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.util.List;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Product {
@@ -19,16 +17,28 @@ public class Product {
     @Column(name = "description", nullable = true)
     private String description;
 
-    @Column(name = "imagePaths", nullable = true)
-    private List<String> imagePaths;
+    @Column(name = "imagePath", nullable = true)
+    private String imagePath;
 
     @Column(name = "quantity", nullable = false)
     private int quantity;
 
+    @Column(name = "local", nullable = true)
+    private String local;
+
+    @ManyToMany
+    @JoinTable(
+        name = "product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
     protected Product() {}
 
-    public Product(String name) {
+    public Product(String name, int quantity) {
         this.name = name;
+        this.quantity = quantity;
     }
 
     public Long getId() {
@@ -51,7 +61,11 @@ public class Product {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public String getLocal() {
+        return local;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
     }
 }
