e1725c06f5aa7db6950d651141172b05cfe2c5c3
Vinícius de Souza Oliveira <viniso1903@gmail.com>
2026-01-14 13:14:18 -0300

    Create PersonService.java

---
+++ src/main/java/com/vinilicia/catalog_backend/domain/service/PersonService.java
@@ -0,0 +1,51 @@
+package com.vinilicia.catalog_backend.domain.service;
+
+import com.vinilicia.catalog_backend.domain.model.Person;
+import com.vinilicia.catalog_backend.domain.repository.PersonRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Transactional
+    public void createPerson(String name) {
+        personRepository.save(new Person(name));
+    }
+
+    @Transactional(readOnly = true)
+    public Person getPerson(Long id) {
+        return personRepository
+            .findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Person not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    @Transactional
+    public void updatePerson(Long id, String name) {
+        Person person = personRepository
+            .findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Person not found"));
+
+        if (name != null) {
+            person.rename(name);
+        }
+    }
+
+    @Transactional
+    public void deletePerson(Long id) {
+        personRepository.findById(id).ifPresent(personRepository::delete);
+    }
+}
